3e83960d58f1b0c5f87d78363f60df34e1c8b295
Carlos Eduardo <ti.carloseduardo@gmail.com>
2025-05-09 19:29:51 -0300

    criacao das pastas projeto algoritimos ordenacao

--- MetodosOrdenacao/Program.cs
+++ MetodosOrdenacao/Program.cs
@@ -1,12 +1,130 @@
 ﻿using System;
+using System.Diagnostics; // Namespace necessário para Stopwatch
+
+
+using MetodosOrdenacao.ClassesOrdenacao;
+using MetodosOrdenacao.ExibirVetorOrdenado;
+using MetodosOrdenacao.ClassesOrdenacao.Enum;
+using MetodosOrdenacao.Randomizador;
+using MetodosOrdenacao.Menu;
+
+
 
 namespace MetodosOrdenacao
     {
     internal class Program
+
         {
+        //private static int[] vetorRandomizado; // Tornando-a acessível globalmente e perde independencia vetores
         static void Main(string[] args)
             {
-            Console.WriteLine("Hello World!");
-            }
-        }
-    }
+            MenuOrdenacao menu = new MenuOrdenacao(); // Instancia o menu
+
+            Stopwatch stopwatch = new Stopwatch(); // Instancia o cronômetro
+            RandomizadorVetor randomizar = new RandomizadorVetor();// delclarado aqui para ser acessado nos if e cases.
+                                                                   // acesso global.responsavel por randomizar o vetor
+                                                                   // variaveis private e vetor passado por copia. 
+                                                                   //  int[] vetorRandomizado = randomizar.ObterCopiaVetorRandomizado();//pegar copia vetor randomizado
+                                                                   //private static int[] vetorRandomizado; // Tornando-a acessível globalmente
+                                                                   //ordenacoes(menu e chamada classes e metodos)
+                                                                   //uso do enum no controle menu organizacao e facilidade manutencao
+                                                                   //TipoOrdenacaoEnum tipoOrdenacao;
+                                                                   //TipoOperacaoEnum tipoOperacao;
+
+
+            while (true)
+                {
+                Console.Clear(); // Limpa a tela do console antes de exibir o menu novamente
+                int opcao = menu.ExibirMenuPrincipal(); // Chama o método para exibir o menu
+                // Verifica se a opção pertence ao TipoOrdenacaoEnum
+                if (Enum.IsDefined(typeof(TipoOrdenacaoEnum), opcao))
+                        {
+                        TipoOrdenacaoEnum tipoOrdenacao = (TipoOrdenacaoEnum)opcao;
+                        int[] vetorTeste = randomizar.ObterCopiaVetorRandomizado();
+
+                        Console.WriteLine($"Executando {tipoOrdenacao}...");
+                        stopwatch.Restart();
+
+                        // Chamando método de ordenação específico
+                        switch (tipoOrdenacao)
+                            {
+                            case TipoOrdenacaoEnum.BubbleSort:
+                                BubbleSort bubble = new BubbleSort(vetorTeste);
+                                bubble.Ordenar();
+                                Console.WriteLine("Bubble Sort concluído.");
+                                break;
+
+                            case TipoOrdenacaoEnum.SelectionSort:
+                                SelectionSort selection = new SelectionSort(vetorTeste);
+                                selection.Ordenar();
+                                Console.WriteLine("Selection Sort concluído.");
+                                break;
+
+                            case TipoOrdenacaoEnum.InsertionSort:
+                                InsertionSort insertion = new InsertionSort(vetorTeste);
+                                insertion.Ordenar();
+                                Console.WriteLine("Insertion Sort concluído.");
+                                break;
+
+                            case TipoOrdenacaoEnum.MergeSort:
+                                MergeSort merge = new MergeSort(vetorTeste);
+                                merge.Ordenar();
+                                Console.WriteLine("Merge Sort Concluido");
+                            break;
+
+                            case TipoOrdenacaoEnum.QuickSort:
+                                QuickSort quick = new QuickSort(vetorTeste);
+                                quick.Ordenar();    
+                            Console.WriteLine("Quick Sort Concluido");
+                                break;
+                            }
+
+                        stopwatch.Stop();
+                        Console.WriteLine($"\nTempo de execução: {stopwatch.Elapsed.TotalSeconds} segundos");
+                        }
+                    // Verifica se a opção pertence ao TipoOperacaoEnum
+                    else if (Enum.IsDefined(typeof(TipoOperacaoEnum), opcao))
+                        {
+                        TipoOperacaoEnum tipoOperacao = (TipoOperacaoEnum)opcao;
+
+                        switch (tipoOperacao)
+                            {
+                            case TipoOperacaoEnum.ImprimirVetor:
+                                Console.WriteLine("Imprimindo vetor de testes...");
+                                if (randomizar != null)
+                                    {
+                                    randomizar.ExibirVetor();
+                                    }
+                                else
+                                    {
+                                    Console.WriteLine("Nenhum vetor foi inicializado! Crie um vetor primeiro.");
+                                    }
+                                break;
+
+                        case TipoOperacaoEnum.GerarNovoVetor:
+                                Console.WriteLine("Gerando novo vetor...");
+                                randomizar.LerTamanhoVetor();
+                                randomizar.Randomize();
+                                Console.WriteLine("Novo vetor gerado com sucesso!");
+                                break;
+
+                            case TipoOperacaoEnum.Sair:
+                                Console.WriteLine("Saindo do programa opcao menu!...");
+                                return; // Sai do loop e encerra o programa
+                            }
+                        }
+                    else
+                        {
+                        Console.WriteLine("Opção inválida! Tente novamente.");
+                    }
+                    if (opcao != (int)TipoOperacaoEnum.Sair) // Evita a pausa ao sair
+                        {
+                        Console.WriteLine("\nPressione qualquer tecla para continuar...");
+                        Console.ReadKey();
+                        }
+            }//while (true);
+        }//Main
+    }//class program 
+} //namespace  
+
+               
---
+++ MetodosOrdenacao/Randomizador/RandomizadorVetor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace MetodosOrdenacao.Randomizador
+    {
+    class RandomizadorVetor
+        {
+        private int[] vetor;
+        private System.Random random;
+
+        public RandomizadorVetor(int tamanho)
+        {
+            if (tamanho <= 0)
+                { 
+                    Console.WriteLine(" Digite positivo maior que zero.");
+                    return;
+                }
+                vetor = new int[tamanho];//criando as instancias
+            random = new System.Random();
+        }
+        
+        public void Randomize() 
+        { 
+            if(vetor == null){
+                Console.WriteLine("Vetor nao foi inicializado(classeRandomizadorVetor");
+                return;
+            }
+                for (int i = 0; i < vetor.Length; i++){ 
+                    vetor[i] = random.Next(100);//ajustar para quantidade numeros que usuario pedir
+                }
+        }
+        public void ExebirVetor()
+        {
+            Console.WriteLine("vetor randomizado");
+            for (int i = 0; i < vetor.Length; i++) {
+                Console.WriteLine(vetor[i] + " ");
+                }
+            Console.WriteLine();
+        }
+        
+    }
+}
